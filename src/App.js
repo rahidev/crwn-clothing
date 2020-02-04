999b07ddb8cd135e66505bc7a119492efa2e2b20
codewithrahi <rahiomkar089@gmail.com>
2020-02-04 14:45:23 +0530

    formatted code

--- src/App.js
+++ src/App.js
@@ -13,15 +13,14 @@ import AuthPage from './pages/auth-page/auth-page.component';
 import './App.css';
 
 const App = () => (
-  <div>
-    <Header />
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={Shop} />
-      <Route exact path='/signin' component={AuthPage} />
-    </Switch>
-
-  </div>
+    <div>
+        <Header />
+        <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/shop" component={Shop} />
+            <Route exact path="/signin" component={AuthPage} />
+        </Switch>
+    </div>
 );
 
 export default App;
