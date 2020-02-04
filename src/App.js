1267c9f79f2287eb8ab119f4598dc8308aa98159
codewithrahi <rahiomkar089@gmail.com>
2020-02-04 12:49:46 +0530

    Adding shop page and collections-preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+import Shop from './pages/shop/shop.component';
 
 import './App.css';
 
 const App = () => (
   <div>
-    <Route exact path='/' component={HomePage} />
-<Route exact path='/shop/:product' render={({match}) => {
-  console.log(match);
-  return <h1>shop for {match.params.product}</h1>
-}} />
+
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={Shop} />
+    </Switch>
+
   </div>
 );
 
